ee45e19f00563d5b7c31409f59aede5c277283a8
Rob Hitt <robhitt@gmail.com>
2017-01-21 23:44:07 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt() {
+  // The function should bind a click event to the p tag. When the paragraph is clicked, the function should alert "Hey!".
+  $('p').on('click', function () {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('you have pressed the g key')
+     }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+    return
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
